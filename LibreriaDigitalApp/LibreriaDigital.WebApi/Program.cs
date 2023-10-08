92a27b8c4c97a1df32823a035640ed5a392ad130
luzadriz <adrianita9403@gmail.com>
2023-10-07 22:38:06 -0500

    nuevos cambios

--- LibreriaDigitalApp/LibreriaDigital.WebApi/Program.cs
+++ LibreriaDigitalApp/LibreriaDigital.WebApi/Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+   app.UseSwagger();
+  app.UseSwaggerUI();
+//}
 app.UseRouting();
 app.UseEndpoints(Endpoint =>
 {
